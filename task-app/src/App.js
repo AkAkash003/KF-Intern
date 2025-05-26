7bb84af4d4edce1f805907240690cda121a9750e
AkAkash003 <akashak2k3@gmail.com>
2025-05-26 15:05:25 +0530

    app.js file modified

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
+
 const techStack = [
   { name: 'React', logo: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg' },
   { name: 'Node.js', logo: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg' },
   { name: 'Express.js', logo: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png' }
 ];
 
-const TechStackList = () => (
-  <ul>
-    {techStack.map((tech, i) => (
-      <li key={i}>
-        <img src={tech.logo} alt={tech.name} width="30" style={{ marginRight: '10px' }} />
-        {tech.name}
-      </li>
-    ))}
-  </ul>
-);
 
-const UpdateMessage = () => {
+function TechStackList() {
+  return (
+    <ul>
+      {techStack.map((tech, index) => (
+        <li key={index}>
+          <img src={tech.logo} alt={tech.name} width="30" /> {tech.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+
+function UpdateMessage() {
   const [msg, setMsg] = useState("I am learning React");
 
   return (
@@ -27,34 +31,43 @@ const UpdateMessage = () => {
       <button onClick={() => setMsg("I am learning React and Hooks as well")}>Update Me</button>
     </div>
   );
-};
+}
 
-const Home = () => <p>This is Home page</p>;
-const Blog = () => <p>This is Blog page</p>;
-const Contact = () => <p>This is Contact Us page</p>;
 
-const App = () => (
-  <Router>
-    <div style={{ padding: '20px' }}>
-      <nav>
-        <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
-        <Link to="/blog" style={{ marginRight: '10px' }}>Blog</Link>
-        <Link to="/contact">Contact Us</Link>
-      </nav>
+function Home() {
+  return <p>This is Home page</p>;
+}
+function Blog() {
+  return <p>This is Blog page</p>;
+}
+function Contact() {
+  return <p>This is Contact Us page</p>;
+}
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
 
-      <h3>Tech Stack</h3>
-      <TechStackList />
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/blog">Blog</Link> |{" "}
+          <Link to="/contact">Contact Us</Link>
+        </nav>
 
-      <UpdateMessage />
-    </div>
-  </Router>
-);
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        <h3>Stack</h3>
+        <TechStackList />
+
+        <UpdateMessage />
+      </div>
+    </Router>
+  );
+}
 
 export default App;
-
